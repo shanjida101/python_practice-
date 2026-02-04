ceb9b78a5cc08c5288a1e9fbb7bb4ef326c4d70d
Your Name <your-email@example.com>
2026-02-04 18:37:25 +0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,14 +8,16 @@ if operation == '+':
     print(result)
 elif operation == '-':
     result = num1 - num2
-    print(f"{num1} - {num2} = {result}") 
+    print(f"{num1} - {num2} = {round(result)}") 
 
 elif operation == '*': 
      result = num1*num2   
-     print(f"{num1} * {num2} = {result}")
+     print(f"{num1} * {num2} = {round(result,5)}")
 elif operation == '/':
     if num2 != 0:
         result = num1 / num2
         print(f"{num1} / {num2} = {result}")
     else:
         print("Error: Division by zero is not allowed.")                 
+else:
+    print(f"Invalid operation: {operation}")
