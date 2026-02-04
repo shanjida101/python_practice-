eef24827b9d425a954fe9a5c017ceeb23ec60520
Your Name <your-email@example.com>
2026-02-04 17:36:59 +0600

    Initial commit: Add Mad Libs game and main script

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+#madlibs game
+#word game where you create a story by filling in the blanks with different types of words (nouns, verbs, adjectives, etc.)
+
+print("Welcome to the Mad Libs Game!")
+
+noun1 = input("Enter a noun(name,place or thing): ")
+verb1 = input("Enter a verb(ending with ing): ")   
+adjective1 = input("Enter an adjective(Description): ")
+
+
+print("Once upon a time, there was a " + adjective1)
+print(f"{noun1} who loved munching on snacks.")
+print("Every day, " + noun1 + " would go " + verb1 + " in the park.")
---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+name = input("What is your name? ")
+print("Nice to meet you, " + name + "!")
+
+age = int(input("How old are you? "))
+if age < 18:
+    print("You are a minor.")
+print("your age is " + str(age)) 
+
+print(type(age))
+ 
+ #type cast convert variabl from one data type to another data type
+ #str() int() float() bool() 
